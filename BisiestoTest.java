fc5855bb5b8acd3ba84641a6a198cc5603907678
Carlos Moron Barrios <cmoron@zeta06.aulas.gsyc.urjc.es>
2018-11-26 13:02:43 +0100

    Realizado el fichero NUEVOS-FALLOS-BISIESTO

--- BisiestoTest.java
+++ BisiestoTest.java
@@ -1,47 +1,44 @@
-//
-// Instrucciones de compilación:
-// javac -cp .:./hamcrest-core-1.3.jar:./junit-4.12.jar MinTest.java
-//
-
-import static org.junit.Assert.*; // En este paquete están los métodos assertTrue, assertEquals, assertNull, fail, etc.
+import static org.junit.Assert.*;
 import org.junit.*;
-import java.util.*;
 
 public class BisiestoTest
 {
 
-	//Test para comprobar que con año negativo se eleva la excepción.
-	@Test (expected = IllegalArgumentException.class) // Este test decora la anotación @Test con la clase de la excepción esperada.
-	public void testForNegativeYear()
+    private Bisiestos anyo;
+
+    @Before      // Set up - Called before every test method.
+    public void setUp()
+    {
+       anyo = new Bisiestos();
+    }
+
+	@Test public void testAnyoCero()
 	{
-		Bisiestos.esBisiesto(-1);
+		assertTrue("Es bisiesto cero", anyo.esBisiesto(0));
 	}
 
-	//Test para comprobar que año 0 no es bisiesto
-	@Test
-	public void testForZeroYear()
+	@Test public void testAnyoPositivo()
 	{
-		assertFalse(Bisiestos.esBisiesto(0));
+		assertTrue("Es bisiesto 1", false == anyo.esBisiesto(1));
 	}
 
-	//Test para comprobar año múltiplo de 4
-	@Test
-	public void testMult4()
+	@Test public void testAnyoNegativo()
 	{
-		assertTrue(Bisiestos.esBisiesto(4));
+		assertTrue("Es bisiesto -1", false == anyo.esBisiesto(-1));
 	}
 
-	//Test para comprobar año múltiplo de 100 pero no de 400
-	@Test
-	public void testMult100()
+	@Test public void testAnyoCien()
 	{
-		assertFalse(Bisiestos.esBisiesto(100));
+		assertTrue("Es bisiesto 100", false == anyo.esBisiesto(100));
 	}
 
-	//Test para comprobar año múltiplo de 400
-	@Test
-	public void testMult400()
+	@Test public void testAnyoCuatro()
 	{
-		assertTrue(Bisiestos.esBisiesto(400));
+		assertTrue("Es bisiesto 4", true == anyo.esBisiesto(4));
+	}
+
+	@Test public void testAnyoCuatrocientos()
+	{
+		assertTrue("Es bisiesto 400", true == anyo.esBisiesto(400));
 	}
 }
